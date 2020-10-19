95fd6aeb1ddd1b214bf3826bd520f6d7da789ddb
Nate <37554478+servusDei2018@users.noreply.github.com>
2020-10-19 18:14:11 -0400

    Working on TUI

--- main.go
+++ main.go
@@ -3,11 +3,59 @@ package main
 import (
 	"fmt"
 	"path"
+	"strings"
+
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
 )
 
 func main() {
+	app := tview.NewApplication()
+	table := tview.NewTable().SetBorders(true)
+	lorem := strings.Split("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", " ")
+	cols, rows := 10, 40
+	word := 0
+	for r := 0; r < rows; r++ {
+		table.SetCell(r, 0,
+			tview.NewTableCell(fmt.Sprintf("%d", r+1)).
+				SetTextColor(tcell.ColorYellow).
+				SetAlign(tview.AlignCenter))
+		for c := 1; c < cols; c++ {
+			table.SetCell(r, c,
+				tview.NewTableCell(lorem[word]).
+					SetTextColor(tcell.ColorWhite).
+					SetAlign(tview.AlignCenter))
+			word = (word + 1) % len(lorem)
+		}
+	}
+	table.Select(0, 0).SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEscape {
+			app.Stop()
+		}
+	})
+	table.SetSelectable(true, true).SetBorder(true).SetTitle("Tandem")
+
+	definition := tview.NewList()
+	pages := tview.NewPages()
+	pages.AddPage("main", table.SetSelectedFunc(func(row int, column int) {
+		
+		pages.SwitchToPage("define")
+	}), true, true)
+	pages.AddPage("define", definition.AddItem("Quit", "Press to exit", 'q', func() {
+		pages.SwitchToPage("main")
+	}), true, false)
+
+	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+}
+
+// Do tests (optionally test the API too)
+func Tests(API bool) {
 	Test("noun");Test("pronoun");Test("verb");Test("adjective");Test("adverb")
-	//TestAPI("nautam");TestAPI("noster");TestAPI("hortari");TestAPI("acri");TestAPI("diligenter")
+	if API {
+		TestAPI("nautam");TestAPI("noster");TestAPI("hortari");TestAPI("acri");TestAPI("diligenter")
+	}
 	fmt.Println("")
 }
 
